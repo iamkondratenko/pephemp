832ff4098e609466a085239a9b9e19f104dceb38
iamkondratenko <45318529+iamkondratenko@users.noreply.github.com>
2020-08-13 17:41:15 +0300

    blog done

--- storage/cms/cache/81/fc/blog.htm.php
+++ storage/cms/cache/81/fc/blog.htm.php
@@ -1,5 +1,5 @@
 <?php 
-class Cms5f34d60609944052790517_ae8941b0a28902a7b5c49c511d602b8bClass extends Cms\Classes\PartialCode
+class Cms5f351db1b7677120239327_43a5a6ac0d753d23ad7d7b97101715d4Class extends Cms\Classes\PartialCode
 {
 
 }
